aeedea684a3ef839c47746ac848ae9598ec71c63
Vatsal Manot <vatsal.manot@yahoo.com>
2021-04-11 01:50:43 +0530

    Update package

--- Sources/Intermodular/Helpers/SwiftSyntax/String+SwiftSyntax.swift
+++ Sources/Intermodular/Helpers/SwiftSyntax/String+SwiftSyntax.swift
@@ -23,29 +23,4 @@ extension String {
     }
 }
 
-extension Range where Bound == AbsolutePosition {
-    public init?(_ range: Range<String.Index>, in string: String) {
-        guard let utf8Range = NSRange(utf8Range: range, in: string) else {
-            return nil
-        }
-        
-        self.init(
-            lowerBound: .init(utf8Offset: utf8Range.lowerBound),
-            upperBound: .init(utf8Offset: utf8Range.upperBound)
-        )
-    }
-}
-
-extension Range where Bound == String.Index {
-    public init?(_ range: Range<AbsolutePosition>, in string: String) {
-        self.init(
-            NSRange(
-                lowerBound: range.lowerBound.utf8Offset,
-                upperBound: range.upperBound.utf8Offset
-            ),
-            in: string
-        )
-    }
-}
-
 #endif
